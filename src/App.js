a357b2c23567d711d91546e9dd5c41cc60272fc2
Ali Demirci <ademirci@phinion.com>
2022-07-11 16:26:04 +0200

    Area-chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  Chart,
+  ChartArea,
+  ChartAxis,
+  ChartGroup,
+  ChartThemeColor,
+  ChartVoronoiContainer,
+
+} from '@patternfly/react-charts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="chart-container">
+      <Chart
+        height={250}
+        width={600}
+        //use padding if labels used <ChartAxis></ChartAxis>
+        padding={{
+          bottom: 75,
+          left: 75,
+          right: 200,
+          top: 50
+        }}
+        // to add legends use this dont forget to update right padding 
+        legendOrientation="vertical"
+        legendPosition="right"
+        legendData={[
+          { name: 'Cats' },
+          { name: 'Dogs' },
+          { name: 'Birds' },
+        ]}
+        //to get tooltip data on the chart use this
+        containerComponent={
+          <ChartVoronoiContainer
+            constrainToVisibleArea
+            labels={({ datum }) => `${datum.name}: ${datum.y}`}
+          />
+        }
+        // use this code to change chart color green,multi,blue
+        themeColor={ChartThemeColor.multi}
+      >
+        {/* Add here to labels x-y for chart */}
+        <ChartAxis label="Years" />
+        <ChartAxis dependentAxis showGrid label="Percentage" />
+        <ChartGroup>
+          <ChartArea
+            data={[
+              { name: 'Cats', x: '2015', y: 3 },
+              { name: 'Cats', x: '2016', y: 4 },
+              { name: 'Cats', x: '2017', y: 8 },
+              { name: 'Cats', x: '2018', y: 6 }
+            ]}
+            interpolation="monotoneX"
+          />
+          <ChartArea
+            data={[
+              { name: 'Dogs', x: '2015', y: 2 },
+              { name: 'Dogs', x: '2016', y: 3 },
+              { name: 'Dogs', x: '2017', y: 4 },
+              { name: 'Dogs', x: '2018', y: 5 },
+              { name: 'Dogs', x: '2019', y: 6 }
+            ]}
+            interpolation="monotoneX"
+          />
+          <ChartArea
+            data={[
+              { name: 'Birds', x: '2015', y: 1 },
+              { name: 'Birds', x: '2016', y: 2 },
+              { name: 'Birds', x: '2017', y: 3 },
+              { name: 'Birds', x: '2018', y: 2 },
+              { name: 'Birds', x: '2019', y: 4 }
+            ]}
+            interpolation="monotoneX"
+          />
+        </ChartGroup>
+
+      </Chart>
     </div>
   );
 }
