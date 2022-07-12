fcbba02e970797840738a7b55735a4e8ea738c3f
Ali Demirci <ademirci@phinion.com>
2022-07-12 09:54:19 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import { BarChart } from './BarChart';
 import { BulletChart } from './BulletChart';
 
 function App() {
-  return <div style={{display:"flex", justifyContent:"space-between"}}>
-    <div className="chart-container">
-<AreaChart/>
-    </div>
-    <div className='chart-container'>
-<BarChart></BarChart>
-    </div>
-    <div className="chart-container"> 
-    <BulletChart></BulletChart></div>
+    return <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div className="chart-container">
+            <AreaChart />
+        </div>
+        <div className='chart-container'>
+            <BarChart></BarChart>
+        </div>
+        <div className="chart-container">
+            <BulletChart></BulletChart></div>
     </div>
 }
 
